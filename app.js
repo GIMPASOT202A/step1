1d64a3a13b2ed9107f5bc1c43f5d459a190958f2
silsgah <gahsilas@gmail.com>
2020-03-08 09:01:26 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const debug = require('debug')('app');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/boostrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/boostrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav: ['Books', 'Authors'],
+      title: 'Library',
+    },
+  );
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  debug(`Listeing on port ${port}`);
+});
